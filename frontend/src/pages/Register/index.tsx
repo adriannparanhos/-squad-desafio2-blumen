1abc45a78d251d8d14867fb7cc8d91ae4ff3ca36
thethoomm <thomas.santos02209@gmail.com>
2024-12-30 11:10:07 -0300

    feat: create register page

---
+++ frontend/src/pages/Register/index.tsx
@@ -0,0 +1,14 @@
+import { RegisterPlantForm } from "./RegisterPlantForm";
+import './styles.css'
+
+export default function Register() {
+  return (
+    <div className="register-page">
+      <div className="form-section">
+        <h2 className="register-title inter">Plant registration</h2>
+        <RegisterPlantForm />
+      </div>
+      <img className="register-plant" src="/images/main-plant.png" alt="Main plant" />
+    </div>
+  );
+}
